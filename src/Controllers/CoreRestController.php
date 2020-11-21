698928f534c8864b086587c8bd865864882e5451
Angger @HP-Ubuntu-20.04 <anggerpputro@gmail.com>
2020-11-21 08:34:18 +0700

    buat jadi lebih flexible (add core rest proxy controller)

--- src/Controllers/CoreRestController.php
+++ src/Controllers/CoreRestController.php
@@ -34,20 +34,21 @@ abstract class CoreRestController extends Controller
 
     public function __construct(CoreModel $model = null, CoreService $service = null)
     {
-        $this->model = $model;
+        if(! is_null($model)) {
+            $this->model = $model;
 
-        if (is_null($service)) {
-            $this->service = new CoreService($model, $this->with, $this->orderBy);
-        } else {
-            $this->service = $service;
-            $this->service->setWith($this->with);
-            $this->service->setOrderBy($this->orderBy);
-        }
+            if (is_null($service)) {
+                $this->service = new CoreService($model, $this->with, $this->orderBy);
+            } else {
+                $this->service = $service;
+                $this->service->setWith($this->with);
+                $this->service->setOrderBy($this->orderBy);
+            }
 
-        if (! is_null($model)) {
             $this->modelPrimaryKeyName = $model->getKeyName();
             $this->modelTableName = $model->getTable();
         }
+
     }
 
 
