33ecf9f193b656ed06a3d17f09703b319b634b8a
angger@ubuntu-pti-nvme <anggerpputro@gmail.com>
2023-06-12 16:47:19 +0700

    update test case

--- config/gemboot_auth.php
+++ config/gemboot_auth.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'base_url' => env('GEMBOOT_AUTH_BASE_URL', 'http://localhost:3000'),
-    'base_api' => env('GEMBOOT_AUTH_BASE_API', 'http://localhost:3000/api/auth'),
+    'base_url' => env('GEMBOOT_AUTH_BASE_URL', 'http://192.168.0.12:3000'),
+    'base_api' => env('GEMBOOT_AUTH_BASE_API', 'http://192.168.0.12:3000/api/auth'),
 ];
--- config/gemboot_gw.php
+++ config/gemboot_gw.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'base_url' => env('GEMBOOT_GW_BASE_URL', 'https://tirta.pdamkotasmg.co.id:8443/gateway/'),
+    'base_url' => env('GEMBOOT_GW_BASE_URL', 'http://192.168.0.12/'),
 
-    'base_url_auth' => env('GEMBOOT_GW_BASE_URL_AUTH', 'https://tirta.pdamkotasmg.co.id:8443/gateway/auth/'),
+    'base_url_auth' => env('GEMBOOT_GW_BASE_URL_AUTH', 'http://192.168.0.12/auth/'),
 ];
