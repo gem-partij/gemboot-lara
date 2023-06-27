5d1ce90ce8fc93ec3517bf064d00faeb31936da7
angger@ubuntu-pti-nvme <anggerpputro@gmail.com>
2023-06-27 15:33:35 +0700

    fix test case

--- tests/Unit/GembootHttpTest.php
+++ tests/Unit/GembootHttpTest.php
@@ -1,36 +1,40 @@
 <?php
+
 namespace Gemboot\Tests\Unit;
 
 use Gemboot\Tests\TestCase;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Gemboot\Tests\Models\TestUser;
 
-class GembootControllerTest extends TestCase {
+class GembootHttpTest extends TestCase
+{
 
     /** ================
      * TEST REQUEST GET ALL DATA (PAGINATION)
      *
      * @test
-    **/
-    public function test_get_all_data() {
+     **/
+    public function test_get_all_data()
+    {
         $response = $this->getJson('/test/users');
         // ob_get_clean();
 
         $response
             ->assertStatus(200)
-            ->assertJson(fn (AssertableJson $json) =>
+            ->assertJson(
+                fn (AssertableJson $json) =>
                 $json
-                ->has('status')
-                ->has('message')
-                ->has('data')
-                ->has('data.current_page')
-                ->has('data.data')
-                ->whereType('data.data', ['array', 'null'])
-                ->has('data.from')
-                ->has('data.last_page')
-                ->has('data.per_page')
-                ->has('data.to')
-                ->has('data.total')
+                    ->has('status')
+                    ->has('message')
+                    ->has('data')
+                    ->has('data.current_page')
+                    ->has('data.data')
+                    ->whereType('data.data', ['array', 'null'])
+                    ->has('data.from')
+                    ->has('data.last_page')
+                    ->has('data.per_page')
+                    ->has('data.to')
+                    ->has('data.total')
             );
     }
 
@@ -38,8 +42,9 @@ class GembootControllerTest extends TestCase {
      * TEST REQUEST STORE DATA
      *
      * @test
-    **/
-    public function test_store_data() {
+     **/
+    public function test_store_data()
+    {
         $data = TestUser::factory()->make();
 
         $response = $this->postJson('/test/users', $data->toArray());
@@ -47,15 +52,16 @@ class GembootControllerTest extends TestCase {
 
         $response
             ->assertStatus(200)
-            ->assertJson(fn (AssertableJson $json) =>
+            ->assertJson(
+                fn (AssertableJson $json) =>
                 $json
-                ->has('status')
-                ->has('message')
-                ->has('data')
-                ->has('data.saved')
-                ->has('data.saved.id')
-                ->has('data.saved.name')
-                ->has('data.saved.email')
+                    ->has('status')
+                    ->has('message')
+                    ->has('data')
+                    ->has('data.saved')
+                    ->has('data.saved.id')
+                    ->has('data.saved.name')
+                    ->has('data.saved.email')
             );
     }
 
@@ -63,23 +69,25 @@ class GembootControllerTest extends TestCase {
      * TEST REQUEST SHOW DATA BY ID
      *
      * @test
-    **/
-    public function test_show_data_by_id() {
+     **/
+    public function test_show_data_by_id()
+    {
         $data = TestUser::factory()->create();
 
-        $response = $this->getJson('/test/users/'.$data->id);
+        $response = $this->getJson('/test/users/' . $data->id);
         // ob_get_clean();
 
         $response
             ->assertStatus(200)
-            ->assertJson(fn (AssertableJson $json) =>
+            ->assertJson(
+                fn (AssertableJson $json) =>
                 $json
-                ->has('status')
-                ->has('message')
-                ->has('data')
-                ->has('data.id')
-                ->has('data.name')
-                ->has('data.email')
+                    ->has('status')
+                    ->has('message')
+                    ->has('data')
+                    ->has('data.id')
+                    ->has('data.name')
+                    ->has('data.email')
             );
     }
 
@@ -87,25 +95,27 @@ class GembootControllerTest extends TestCase {
      * TEST REQUEST UPDATE DATA
      *
      * @test
-    **/
-    public function test_update_data() {
+     **/
+    public function test_update_data()
+    {
         $data = TestUser::factory()->create();
         $data_update = TestUser::factory()->make();
 
-        $response = $this->putJson('/test/users/'.$data->id, $data_update->toArray());
+        $response = $this->putJson('/test/users/' . $data->id, $data_update->toArray());
         // ob_get_clean();
 
         $response
             ->assertStatus(200)
-            ->assertJson(fn (AssertableJson $json) =>
+            ->assertJson(
+                fn (AssertableJson $json) =>
                 $json
-                ->has('status')
-                ->has('message')
-                ->has('data')
-                ->has('data.saved')
-                ->has('data.saved.id')
-                ->has('data.saved.name')
-                ->has('data.saved.email')
+                    ->has('status')
+                    ->has('message')
+                    ->has('data')
+                    ->has('data.saved')
+                    ->has('data.saved.id')
+                    ->has('data.saved.name')
+                    ->has('data.saved.email')
             );
     }
 
@@ -113,25 +123,26 @@ class GembootControllerTest extends TestCase {
      * TEST REQUEST DELETE DATA
      *
      * @test
-    **/
-    public function test_delete_data() {
+     **/
+    public function test_delete_data()
+    {
         $data = TestUser::factory()->create();
 
-        $response = $this->deleteJson('/test/users/'.$data->id);
+        $response = $this->deleteJson('/test/users/' . $data->id);
         // ob_get_clean();
 
         $response
             ->assertStatus(200)
-            ->assertJson(fn (AssertableJson $json) =>
+            ->assertJson(
+                fn (AssertableJson $json) =>
                 $json
-                ->has('status')
-                ->has('message')
-                ->has('data')
-                ->has('data.deleted')
-                ->has('data.deleted.id')
-                ->has('data.deleted.name')
-                ->has('data.deleted.email')
+                    ->has('status')
+                    ->has('message')
+                    ->has('data')
+                    ->has('data.deleted')
+                    ->has('data.deleted.id')
+                    ->has('data.deleted.name')
+                    ->has('data.deleted.email')
             );
     }
-
 }
